8ea692f2087a78b7b5e449361c8c05079b632c00
Sedigheh Mohammadkhani <semohammadkhani@gmail.com>
2024-09-19 09:57:32 -0600

    Fix some small issues

--- flatten.js
+++ flatten.js
@@ -2,7 +2,7 @@
 function flatten(arr) {
     let newArray = []
     for (let i = 0; i < arr.length; i++) {
-        if (Array.isArray(arr[0])) {
+        if (Array.isArray(arr[i])) {
             newArray = newArray.concat(flatten(arr[i]))
         } else {
             newArray.push(arr[i])
